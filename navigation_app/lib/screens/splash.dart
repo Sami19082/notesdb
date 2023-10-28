12101ff285d255910833c6222d9cbd9b6ca302f2
samikgp <samidsheikh851@gmail.com>
2023-10-28 14:47:35 +0530

    initial project

--- navigation_app/lib/screens/splash.dart
+++ navigation_app/lib/screens/splash.dart
@@ -10,6 +10,10 @@ class splash extends StatefulWidget {
 class _splashState extends State<splash> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
   }
 }
